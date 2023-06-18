f9f5552a5e93e4d4b0044288ad81b38f06abd9ef
ChamathkaSadamini <chamathkamy@gmail.com>
2023-06-18 19:27:18 +0530

    Updates

---
+++ precache-manifest.8a550904e0f72ad0be455f30f8a38192.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74263b707f80ef0f7c39e0ab194bca3a",
+    "url": "/Weather2/index.html"
+  },
+  {
+    "revision": "282b60026506e1655dd0",
+    "url": "/Weather2/static/css/main.6eeed3fb.chunk.css"
+  },
+  {
+    "revision": "001b67d05a335e326bff",
+    "url": "/Weather2/static/js/2.6a7fe4e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather2/static/js/2.6a7fe4e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "282b60026506e1655dd0",
+    "url": "/Weather2/static/js/main.5b579e1d.chunk.js"
+  },
+  {
+    "revision": "f92457ae7732129a2be5",
+    "url": "/Weather2/static/js/runtime-main.09f86455.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/Weather2/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "75e0f8ca422777deab777d3abe0a90e7",
+    "url": "/Weather2/static/media/background.75e0f8ca.jpg"
+  },
+  {
+    "revision": "e1698332435d6a6fb48294f85157aeab",
+    "url": "/Weather2/static/media/city.e1698332.jpg"
+  }
+]);
